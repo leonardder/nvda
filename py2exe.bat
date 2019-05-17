494a254b2a3b1c0dcd4611a1d5a5f38027002aa9
Leonard de Ruijter <leonard@babbage.com>
2019-05-17 15:45:36 +0200

    Update batch script

--- py2exe.bat
+++ py2exe.bat
@@ -1,2 +1,3 @@
 @echo off
-c:\python37\python.exe source\setup.py build --build-base build py2exe --dist-dir dist 
+cd source
+c:\python37\python.exe setup.py build --build-base ..\build py2exe --dist-dir ..\dist 
