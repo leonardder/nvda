5a44ffa6e502a05c45d88dfdd5e15fdcd28b7fdd
Leonard de Ruijter <leonard@babbage.com>
2019-06-05 19:49:17 +0200

    More work, far from ready

--- source/hwIo/ftdi2.py
+++ source/hwIo/ftdi2.py
@@ -11,6 +11,7 @@ try:
 except WindowsError:
 	raise RuntimeError("ftd2xx library not available")
 from . import IoBase, _isDebug
+from serial.win32 import FILE_FLAG_OVERLAPPED, FILE_ATTRIBUTE_NORMAL, ResetEvent, COMSTAT
 
 FT_OK = 0
 FT_LIST_NUMBER_ONLY = 0x80000000
@@ -21,12 +22,12 @@ FT_PURGE_RX = 1
 FT_PURGE_TX = 2
 
 class FtdiBitModes:
-    RESET         = 0x0
-    ASYNC_BITBANG = 0x1
-    MPSSE         = 0x2
-    SYNC_BITBANG  = 0x4
-    MCU_HOST      = 0x8
-    FAST_SERIAL   = 0x10
+	RESET		 = 0x0
+	ASYNC_BITBANG = 0x1
+	MPSSE		 = 0x2
+	SYNC_BITBANG  = 0x4
+	MCU_HOST	  = 0x8
+	FAST_SERIAL   = 0x10
 
 class DeviceListInfoNode(c.Structure):
 	_fields_ = [
@@ -39,7 +40,7 @@ class DeviceListInfoNode(c.Structure):
 		('none',c.c_void_p),
 	]
 
-class Ftdi2(IoBase):
+class Ftdi2(Iobase):
 	"""Raw I/O for FTDI serial devices.
 	By default, these devices don't offer a serial COM port.
 	"""
@@ -53,10 +54,76 @@ class Ftdi2(IoBase):
 		"""
 		if _isDebug():
 			log.debug("Opening FTDI device %s" % serial)
-		handle = ctypes.c_ulong()
-		res = ftd2xx.FT_OpenEx(serial, FT_OPEN_BY_SERIAL_NUMBER, ctypes.byref(handle))
-		if res != FT_OK:
-			raise RuntimeError("FT_OpenEx failed with error code %d" % res)
-		assert handle.value > 0, "Invalid handle"
-		super(Ftdi2, self).__init__(handle, onReceive)
-		
+		handle = ftd2xx.FT_W32_CreateFile(
+			serial,
+			winKernel.GENERIC_READ | winKernel.GENERIC_WRITE,
+			0,
+			None,
+			winKernel.OPEN_EXISTING,
+			FILE_ATTRIBUTE_NORMAL | FILE_FLAG_OVERLAPPED | FT_OPEN_BY_SERIAL_NUMBER,
+			None
+		)
+		if handle == INVALID_HANDLE_VALUE:
+			lastError = ftd2xx.FT_W32_GetLastError()
+			if _isDebug():
+				log.debug("Open failed: %s" % ctypes.WinError(lastError))
+			raise ctypes.WinError(lastError)
+		super(Ftdi2, self).__init__(handle, onReceive, )
+
+	def _asyncRead(self):
+		# Wait for _readSize bytes of data.
+		# _ioDone will call onReceive once it is received.
+		# onReceive can then optionally read additional bytes if it knows these are coming.
+		ftd2xx.ReadFileEx(self._file, self._readBuf, self._readSize, byref(self._readOl), self._ioDoneInst)
+
+	def read(self, size=1):
+		# Adapted from serial.win32.Win32Serial
+		if size > 0:
+			win32.ResetEvent(self._readOl.hEvent)
+			buf = ctypes.create_string_buffer()
+			rc = ctypes.DWORD()
+			err = win32.ReadFile(self.hComPort, buf, n, ctypes.byref(rc), ctypes.byref(self._overlappedRead))
+					if not err and win32.GetLastError() != win32.ERROR_IO_PENDING:
+						raise SerialException("ReadFile failed (%r)" % ctypes.WinError())
+					err = win32.WaitForSingleObject(self._overlappedRead.hEvent, win32.INFINITE)
+					read = buf.raw[:rc.value]
+				else:
+					read = bytes()
+			else:
+				buf = ctypes.create_string_buffer(size)
+				rc = win32.DWORD()
+				err = win32.ReadFile(self.hComPort, buf, size, ctypes.byref(rc), ctypes.byref(self._overlappedRead))
+				if not err and win32.GetLastError() != win32.ERROR_IO_PENDING:
+					raise SerialException("ReadFile failed (%r)" % ctypes.WinError())
+				err = win32.GetOverlappedResult(self.hComPort, ctypes.byref(self._overlappedRead), ctypes.byref(rc), True)
+				read = buf.raw[:rc.value]
+		else:
+			read = bytes()
+		return bytes(read)
+
+	def write(self, data):
+		if _isDebug():
+			log.debug("Write: %r" % data)
+		size = self._writeSize or len(data)
+		buf = ctypes.create_string_buffer(size)
+		buf.raw = data
+		if not ftd2xx.FT_W32_WriteFile(self._writeFile, data, size, None, byref(self._writeOl)):
+			lastError = ftd2xx.FT_W32_GetLastError()
+			if lastError != ERROR_IO_PENDING:
+				if _isDebug():
+					log.debug("Write failed: %s" % ctypes.WinError())
+				raise ctypes.WinError(lastError)
+			bytes = DWORD()
+			ftd2xx.FT_W32_GetOverlappedResult(self._writeFile, byref(self._writeOl), byref(bytes), True)
+
+	def close(self):
+		if not self._ser:
+			return
+		super(Serial, self).close()
+		self._ser.close()
+
+	def _notifyReceive(self, data):
+		# Set the timeout for onReceive in case it does a sync read.
+		self._setTimeout(self._origTimeout)
+		super(Serial, self)._notifyReceive(data)
+		self._setTimeout(None)
