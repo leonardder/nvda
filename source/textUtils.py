e4176e5a23b0e26250136167d19216edc8d7e59d
Leonard de Ruijter <leonard@babbage.com>
2019-06-11 15:29:53 +0200

    Change textUtils approach

--- source/textUtils.py
+++ source/textUtils.py
@@ -5,32 +5,25 @@
 #Copyright (C) 2018-2019 NV Access Limited, Babbage B.V.
 
 """
-Classes and utilities to deal with strings.
+Classes and utilities to deal with variable width and multi byte encodings.
 """
 
 import encodings
 import sys
-from collections.abc import Sequence, ByteString
+try:
+	from collections.abc import ByteString # Python 3 import
+except ImportError:
+	from six import binary_type as ByteString
+	from six import text_type as str
 
-defaultStringEncoding = "utf_32_le"
-
-class EncodingAwareString(Sequence):
+class EncodingAwareString(object):
 	"""
 	Object that holds a string in both its decoded and its UTF-x encoded form.
 	The indexes and length of the resulting objects are based on the byte size of the given encoding.
 
-	First, an instance is created:
+	An instance is created like this:
 
 	>> string = EncodingAwareString('\U0001f609', encoding="utf_16_le")
-
-	Then, the length of the string can be fetched:
-
-	>>> len(string)
-	2
-
-	And the object can be indexed and sliced:
-
-	This example string behaves exactly the same as a Python 2 unicode string, which is stored using a two bytes encoding.
 	"""
 
 	__slots__=("bytesPerIndex", "decoded", "encoded", "encoding", "errors")
@@ -64,16 +57,25 @@ class EncodingAwareString(Sequence):
 	def __repr__(self):
 		return "{}({}, encoding={})".format(self.__class__.__name__, repr(self.decoded), self.encoding)
 
-	def __mul__(self, value):
-		return EncodingAwareString(self.decoded.__mul__(value), self.encoding, self.errors)
-
-	def __rmul__(self, value):
-		return EncodingAwareString(self.decoded.__rmul__(value), self.encoding, self.errors)
-
-	def __len__(self):
+	@property
+	def encodingAwareLength(self):
 		return len(self.encoded) // self.bytesPerIndex
 
-	def __getitem__(self, key):
+	def getEncodingAwareOffsets(self, bytesStart, bytesEnd):
+		if bytesStart >= len(self.encoded) or bytesEnd >= len(self.encoded):
+			raise IndexError("str indexes out of range")
+		if self.bytesPerIndex == 1:
+			return (bytesStart, bytesEnd)
+		return (bytesStart // self.bytesPerIndex, bytesEnd // self.bytesPerIndex)
+
+	def getBytesOffsets(self, encodingAwareStart, encodingAwareEnd):
+		if encodingAwareStart >= self.encodingAwareLength or encodingAwareEnd >= self.encodingAwareLength:
+			raise IndexError("EncodingAwareString indexes out of range")
+		if self.bytesPerIndex == 1:
+			return (encodingAwareStart, encodingAwareEnd)
+		return (encodingAwareStart * self.bytesPerIndex, encodingAwareEnd * self.bytesPerIndex)
+
+	def encodingAwareGetitem(self, key):
 		if isinstance(key, int):
 			if key >= len(self):
 				raise IndexError("%s index out of range" % EncodingAwareString.__name__)
@@ -101,61 +103,11 @@ class EncodingAwareString(Sequence):
 			step = key.step
 			newKey = slice(start, stop, step)
 		else:
-			return NotImplemented
+			raise TypeError("Expected int or slice")
 		return EncodingAwareString(self.encoded[newKey], self.encoding, self.errors)
-
-	def __contains__(self, char):
-		if isinstance(char, ByteString):
-			return char in self.encoded
-		return char in self.decoded
-
-	def count(self, sub, start=0, end=sys.maxsize):
-		if isinstance(sub, str):
-			sub = sub.encode(self.encoding, self.errors)
-		if not isinstance(sub, ByteString):
-			raise TypeError("Expected an object of type str or ByteString")
-		start *= self.bytesPerIndex
-		end = min(end * self.bytesPerIndex, sys.maxsize)
-		return self.encoded.count(sub, start, end) / self.bytesPerIndex
-
-	def find(self, sub, start=0, end=sys.maxsize):
-		if isinstance(sub, str):
-			sub = sub.encode(self.encoding, self.errors)
-		if not isinstance(sub, ByteString):
-			raise TypeError("Expected an object of type str or ByteString")
-		start *= self.bytesPerIndex
-		end = min(end * self.bytesPerIndex, sys.maxsize)
-		return self.encoded.find(sub, start, end) / self.bytesPerIndex
-
-	def index(self, sub, start=0, end=sys.maxsize):
-		if isinstance(sub, str):
-			sub = sub.encode(self.encoding, self.errors)
-		if not isinstance(sub, ByteString):
-			raise TypeError("Expected an object of type str or ByteString")
-		start *= self.bytesPerIndex
-		end = min(end * self.bytesPerIndex, sys.maxsize)
-		return self.encoded.index(sub, start, end) / self.bytesPerIndex
 
 	def joinByteString(self, seq):
 		return EncodingAwareString(self.encoded.join(seq), self.encoding, self.errors)
 
 	def joinStr(self, seq):
 		return EncodingAwareString(self.decoded.join(seq), self.encoding, self.errors)
-
-	def rfind(self, sub, start=0, end=sys.maxsize):
-		if isinstance(sub, str):
-			sub = sub.encode(self.encoding, self.errors)
-		if not isinstance(sub, ByteString):
-			raise TypeError("Expected an object of type str or ByteString")
-		start *= self.bytesPerIndex
-		end = min(end * self.bytesPerIndex, sys.maxsize)
-		return self.encoded.rfind(sub, start, end) / self.bytesPerIndex
-
-	def rindex(self, sub, start=0, end=sys.maxsize):
-		if isinstance(sub, str):
-			sub = sub.encode(self.encoding, self.errors)
-		if not isinstance(sub, ByteString):
-			raise TypeError("Expected an object of type str or ByteString")
-		start *= self.bytesPerIndex
-		end = min(end * self.bytesPerIndex, sys.maxsize)
-		return self.encoded.rindex(sub, start, end) / self.bytesPerIndex
