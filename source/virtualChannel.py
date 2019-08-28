c58571e3638b09e01667c1a2788eacde064a061d
Leonard de Ruijter <alderuijter@gmail.com>
2019-08-28 20:04:33 +0200

    Updates

--- source/virtualChannel.py
+++ source/virtualChannel.py
@@ -1,4 +1,9 @@
+import os
+import sys
+sys.path.append(os.path.dirname(__file__))
 import sourceEnv
+import winsound
+winsound.Beep(440, 300)
 import comtypes.gen
 import comtypesMonkeyPatches
 import comInterfaces
@@ -6,16 +11,28 @@ comtypes.gen.__path__.append(comInterfaces.__path__[0])
 from comInterfaces import NVDAVirtualChannel
 import comtypes
 import comtypes.server.localserver
+from ctypes import POINTER, pointer
 
 class NVDAVirtualChannelPlugin(NVDAVirtualChannel.NVDA):
-	_com_interfaces_ = [
-		NVDAVirtualChannel.IWTSPlugin,
-	]
 	_reg_threading_ = "Free"
+	_reg_desc_ = "NVDA Virtual Channel Plugin Class"
 	_reg_progid_ = "NVDAVirtualChannel.NVDA.1"
 	_reg_novers_progid_ = "NVDAVirtualChannel.NVDA"
 	_reg_clsctx_ = comtypes.CLSCTX_LOCAL_SERVER
 	_regcls_ = comtypes.server.localserver.REGCLS_MULTIPLEUSE
+
+	def Initialize(self, pChannelMgr):
+		listenerCallback = self.QueryInterface(NVDAVirtualChannel.IWTSListenerCallback)
+		self._listener = pChannelMgr.CreateListener("echo", 0, listenerCallback)
+
+	def Connected(self):
+		return
+
+	def Disconnected(dwDisconnectCode):
+		return
+
+	def Terminated(self):
+		return
 
 if __name__ == "__main__":
 	from comtypes.server.register import UseCommandLine
