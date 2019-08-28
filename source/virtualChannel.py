d3c9aa32a7a92ef3baa6c4d27629c44b9813a3e3
Leonard de Ruijter <leonard@babbage.com>
2019-08-28 08:10:01 +0200

    Remove inproc stuff

--- source/virtualChannel.py
+++ source/virtualChannel.py
@@ -14,8 +14,9 @@ class NVDAVirtualChannelPlugin(NVDAVirtualChannel.NVDA):
 	_reg_threading_ = "Free"
 	_reg_progid_ = "NVDAVirtualChannel.NVDA.1"
 	_reg_novers_progid_ = "NVDAVirtualChannel.NVDA"
-	_reg_clsctx_ = comtypes.CLSCTX_INPROC_SERVER | comtypes.CLSCTX_LOCAL_SERVER
+	_reg_clsctx_ = comtypes.CLSCTX_LOCAL_SERVER
 	_regcls_ = comtypes.server.localserver.REGCLS_MULTIPLEUSE
+
 if __name__ == "__main__":
 	from comtypes.server.register import UseCommandLine
 	UseCommandLine(NVDAVirtualChannelPlugin)
