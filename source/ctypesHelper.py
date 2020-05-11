9de15a0cf0837ecfb0f676f7c3b69de258231a4d
Leonard de Ruijter <alderuijter@gmail.com>
2020-05-11 19:42:15 +0200

    Documentation and splitting

--- source/ctypesHelper.py
+++ source/ctypesHelper.py
@@ -7,7 +7,7 @@
 
 import ctypes
 from typing import Any, Callable, Union, Optional, Tuple,get_type_hints
-from inspect import signature, _empty, Parameter
+from inspect import signature, Signature, _empty, Parameter
 from dataclasses import dataclass
 from logHandler import log
 from functools import wraps
@@ -30,8 +30,37 @@ class OutParam:
 	default: Any = _empty
 
 
-def parameterToParamFlagsTuple(param: Parameter) -> Tuple:
-	"""Converts a L{Parameter} to a ctypes paramflags tuple.
+def signatureToFuncType(
+		sig: Signature,
+		funcTypeFactory: Callable
+) -> ctypes._CFuncPtr:
+	"""Creates a C function type from a L{Signature}.
+	@param sig: The signature.
+	@param funcTypeFactory: One of ctypes.CFUNCTYPE, ctypes.WINFUNCTYPE or ctypes.PYFUNCTYPE.
+	"""
+	if sig.return_annotation is _empty:
+		raise AnnotationError(f"Signature {sig} has no return annotation")
+	emptyAnnotations = [
+		name for name, param in sig.parameters.items()
+		if param.annotation is _empty
+	]
+	if emptyAnnotations:
+		emptyAnnotationsStr = ", ".join(emptyAnnotations)
+		raise AnnotationError(f"Signature {sig} has no annotations for parameters {emptyAnnotationsStr}")
+	if any(
+		param for param in sig.parameters.values()
+		if param.kind in (Parameter.VAR_POSITIONAL, Parameter.VAR_KEYWORD)
+	):
+		raise ValueError(
+			f"Signature {sig} contains variable arguments (*args or **kwargs), which is unsupported"
+		)
+	resType = sig.return_annotation
+	argTypes = tuple(param.annotation for param in sig.parameters.values())
+	return funcTypeFactory(resType, *argTypes)
+
+
+def _parameterToParamFlag(param: Parameter) -> Tuple:
+	"""Converts a L{Parameter} to a ctypes paramflag.
 	Any parameter with a defaultvalue of type L{OutParam} will be treated as an output parameter.
 	Any other parameters will be treated as input parameter.
 	See https://docs.python.org/3/library/ctypes.html#function-prototypes for documentation about paramFlags.
@@ -86,34 +115,47 @@ def errCheckFactory(
 def annotatedCFunction(
 		library: ctypes.CDLL,
 		nameOrOrdinal: Optional[Union[str, int]] = None,
-		typeFactory: Callable = ctypes.WINFUNCTYPE,
+		funcTypeFactory: Callable = ctypes.WINFUNCTYPE,
 		*,
-		errCheckCallable: Optional[Callable[[Any], bool]] = lambda res: res in (None, 0),
+		errCheckCallable: Optional[Callable[[Any], bool]] = None,
 		errRaiseCallable: Callable[[], Exception] = ctypes.WinError,
 		discardReturnValue: bool = False
 ):
+	"""Annotated C function decorator.
+
+	This decorator can be used to convert a default python style function to a function that wraps a
+	C function from an external DLL.
+	The decorated function requires type annotations for all its parameters.
+	A FUNCTYPE class will be created from the signature of the function using L{signatureToFuncType},
+	The behavior of the decorator can be finetuned using the following arguments:
+	@param library: The dll that contains the function,
+		e.g. ctypes.windll.user32.
+	@param nameOrOrdinal: The name or ordinal of the exported function,
+		e.g. GetClassNameW.
+		If this value is C{None}, the name of the decorated function will be used.
+	@param funcTypeFactory: One of ctypes.CFUNCTYPE, ctypes.WINFUNCTYPE or ctypes.PYFUNCTYPE.
+	@param errCheckCallable: A callable that receives the result of the function as its only argument,
+		returning C{True} if the result indicates an error, C{False} otherwise.
+		For example, if a result of 0 means an error,
+		the callback could look like: lambda res: res == 0
+		If C{None}, no error check will be performed.
+	@param errRaiseCallable: A callback returning an exception object that will be raised.
+		defaults to ctypes.WinError, which raises the appropriate windows error.
+		if L{errCheckCallable} is C{None}, this parameter has no effect.
+	@param discardReturnValue: discards the return value of the C function.
+		* If C{False} and the function has one or more output parameters, their values are returned.
+		* If C{False} and the function has no output parameters, the return value will be C{None}
+		* If C{True} and the function has one or more output parameters,
+		the return value will be a tuple of the function's result and the output parameter values.
+		* If C{False} and the function has no output parameters,
+		the return value will be the result of the function.
+	"""
+
 	def wrap(func):
-		if not get_type_hints(func):
-			raise AnnotationError(f"{func.__qualname__} stub has no annotations")
 		sig = signature(func)
-		if sig.return_annotation is _empty:
-			raise AnnotationError(f"{func.__qualname__} stub has no return annotation")
-		emptyAnnotations = [name for name, param in sig.parameters.items() if param.annotation is _empty]
-		if emptyAnnotations:
-			emptyAnnotationsStr = ", ".join(emptyAnnotations)
-			raise AnnotationError(f"{func.__qualname__} stub has no annotations for parameters {emptyAnnotationsStr}")
-		if any(
-			param for param in sig.parameters.values()
-			if param.kind in (Parameter.VAR_POSITIONAL, Parameter.VAR_KEYWORD)
-		):
-			raise ValueError(
-				f"{func.__qualname__} contains variable arguments (*args or **kwargs), which is unsupported"
-			)
-		resType = sig.return_annotation
-		argTypes = tuple(param.annotation for param in sig.parameters.values())
-		funcType = typeFactory(resType, *argTypes)
+		funcType = signatureToFuncType(sig, funcTypeFactory)
 		funcSpec = (nameOrOrdinal or func.__name__, library)
-		paramFlags = tuple(parameterToParamFlagsTuple(param) for param in sig.parameters.values())
+		paramFlags = tuple(_parameterToParamFlag(param) for param in sig.parameters.values())
 		funcObj = funcType(funcSpec, paramFlags)
 		funcObj.errcheck = errCheckFactory(
 			paramFlags,
@@ -128,5 +170,6 @@ def annotatedCFunction(
 
 		wrapper.funcSpec = funcSpec
 		wrapper.paramFlags = paramFlags
+		wrapper._cFunc = funcObj
 		return wrapper
 	return wrap
